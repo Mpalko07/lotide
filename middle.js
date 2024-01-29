ce3f0ecdc111c1696f824661b478666b7903e00d
Monica Palko <m.a.palko07@gmail.com>
2024-01-28 22:23:30 -0500

    -a

---
+++ middle.js
@@ -0,0 +1,40 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} is equal to ${array2}`);
+  } else { 
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} is not equal to ${array2}`);
+  }
+};
+
+const middle = function(array) {
+  const length = array.length;
+  const middleIndex = Math.floor(length / 2);
+
+  if (length <= 2) {
+    return [];
+  } else if (length % 2 === 1) {
+    return [array[middleIndex]];
+  } else {
+    return [array[middleIndex - 1], array[middleIndex]];
+  }
+};
+
+// tests
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); 
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
