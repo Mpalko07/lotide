04e040c21a3e05f5695c9b2b09c86d53a50d20ac
Monica Palko <m.a.palko07@gmail.com>
2024-01-28 21:30:33 -0500

    -a

--- without.js
+++ without.js
@@ -38,3 +38,8 @@ const result2 = without(["1", "2", "3"], [1, 2, "3"]);
 
 assertArraysEqual(result1, [2, 3]);
 assertArraysEqual(result2, ["1", "2"]);
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
