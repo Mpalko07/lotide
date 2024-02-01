c919f9db9598f32c3c314fbda82010a7a0ce4c4a
Monica Palko <m.a.palko07@gmail.com>
2024-01-31 23:06:36 -0500

    -a

--- letterPositions.js
+++ letterPositions.js
@@ -1,8 +1,25 @@
+// Helper function for comparing arrays
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+// Helper function for asserting arrays are equal
 const assertArraysEqual = function(actual, expected) {
-  if (actual = expected) {
-    console.log(` ✅✅✅ Assertion Passed: ${actual} === ${expected} `);
+  const isEqual = eqArrays(actual, expected);
+  if (isEqual) {
+    console.log(` ✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(` 🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `);
+    console.log(` 🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
