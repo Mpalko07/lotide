b2aadb7a2ffa293313ac510729a9a2d53ab4094f
Monica Palko <m.a.palko07@gmail.com>
2024-01-31 22:55:32 -0500

    -a

---
+++ letterPositions.js
@@ -0,0 +1,32 @@
+const assertArraysEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(` ✅✅✅ Assertion Passed: ${actual} === ${expected} `);
+  } else {
+    console.log(` 🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i];
+
+    if (letter !== ' ') {
+      if (results[letter]) {
+        results[letter].push(i);
+      } else {
+        results[letter] = [i];
+      }
+    }
+  }
+
+  return results;
+};
+
+// Test
+const result = letterPositions("hello");
+assertArraysEqual(result['h'], [0]);
+assertArraysEqual(result['e'], [1]);
+assertArraysEqual(result['l'], [2, 3]);
+assertArraysEqual(result['o'], [4]);
