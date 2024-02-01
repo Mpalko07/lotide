fbcb851fc6278436c9db651c248d2d70263cd9bd
Monica Palko <m.a.palko07@gmail.com>
2024-01-31 22:01:39 -0500

    -a

--- countLetters.js
+++ countLetters.js
@@ -21,4 +21,14 @@ const countLetters = function(sentence) {
 
 // Test
 const result = countLetters('Is this working');
-console.log(result);
+assertEqual(result['I'], 1);
+assertEqual(result['s'], 1);
+assertEqual(result['t'], 1);
+assertEqual(result['h'], 1);
+assertEqual(result['i'], 1);
+assertEqual(result['w'], 1);
+assertEqual(result['o'], 1);
+assertEqual(result['r'], 1);
+assertEqual(result['k'], 1);
+assertEqual(result['n'], 1);
+assertEqual(result['g'], 1);
