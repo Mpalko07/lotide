7481ff9cc2d97374747f67aaa8384906d5d54917
Monica Palko <m.a.palko07@gmail.com>
2024-02-04 21:04:44 -0500

    -a

---
+++ assertObjectsEqual.js
@@ -0,0 +1,68 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(` ✅✅✅ Assertion Passed: ${actual} === ${expected} `);
+  } else {
+    console.log(` 🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `);
+  }
+};
+
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key of keys1) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      }
+        if (!Array.isArray(object1[key]) && object1[key] !== object2[key]) {
+          return false;
+    }
+  }
+
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+// Tests
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+assertObjectsEqual(shirtObject, anotherShirtObject);
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+assertObjectsEqual(shirtObject, longSleeveShirtObject);
+
+const pantObject = { size: "medium", colors: ["blue", "pink"] };
+const anotherPantObject = { size: "medium", colors: ["blue", "pink"] };
+assertObjectsEqual(pantObject, anotherPantObject);
+
+const shortPantObject = { size: "medium", colors: ["green", "pink"] };
+assertObjectsEqual(pantObject, shortPantObject);
