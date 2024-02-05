bafb10fd03857b28b6488c9aba5ae8e24613c76f
Monica Palko <m.a.palko07@gmail.com>
2024-02-04 20:34:40 -0500

    -a

--- eqObjects.js
+++ eqObjects.js
@@ -29,14 +29,12 @@ const eqObjects = function(object1, object2) {
   }
 
   for (const key of keys1) {
-    if (object1[key] !== object2[key]) {
       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
         if (!eqArrays(object1[key], object2[key])) {
           return false;
         }
-      } else {
+      } else if (object1[key] !== object2[key]) {
         return false;
-      }
     }
   }
 
