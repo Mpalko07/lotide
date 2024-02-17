2426ca80a283077bc936c5088100b1d6fb9f4960
Monica Palko <m.a.palko07@gmail.com>
2024-02-16 22:23:11 -0500

    -a

--- test/test/test/headTest.js
+++ test/test/test/headTest.js
@@ -1,7 +1,7 @@
 // TEST CODE
-const assertEqual = require('../assertEqual');
+const assertEqual = require('../../../assertEqual');
 
-const head = require('../head');
+const head = require('../../../head');
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
