50f73759bc5fa8ab623c892eba3317f8e52ef6eb
Monica Palko <m.a.palko07@gmail.com>
2024-02-17 17:59:07 -0500

    -a

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,25 +1,6 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(` ✅✅✅ Assertion Passed: ${actual} === ${expected} `);
-  } else {
-    console.log(` 🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `);
-  }
-};
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
 
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -29,8 +10,4 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-// TEST CODE
-const arr1 = [1, 2, 3];
-const arr2 = [1, 2, 3];
-
-assertArraysEqual(arr1, arr2);
+module.exports = assertArraysEqual;
--- tail.js
+++ tail.js
@@ -1,30 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(` ✅✅✅ Assertion Passed: ${actual} === ${expected} `);
-  } else {
-    console.log(` 🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
   return array.slice(1);
 };
 
-// Test Case 1: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); 
-assertEqual(words.length, 3); 
-
-// Test Case 2: Check the tail of an array with multiple elements
-const numbers = [1, 2, 3, 4, 5];
-const tailOfNumbers = tail(numbers);
-assertEqual(tailOfNumbers[0], numbers[1]); 
-assertEqual(tailOfNumbers.length, numbers.length - 1); 
-
-// Test Case 3: Check the tail of an array with one element
-const singleElementArray = ["OnlyElement"];
-assertEqual(tail(singleElementArray).length, 0);
-
-// Test Case 4: Check the tail of an empty array
-const emptyArray = [];
-assertEqual(tail(emptyArray).length, 0);
+module.exports = tail;
