e508868dccd6de80a17b670469ccc97adbd6cd2a
Monica Palko <m.a.palko07@gmail.com>
2024-02-16 22:28:27 -0500

    -a

--- test/test_assertEqualTest.js
+++ test/test_assertEqualTest.js
@@ -1,6 +1,6 @@
 // TEST CODE FROM assertEqual.js
 
-const assertEqual = require('../../../assertEqual');
+const assertEqual = require('../assertEqual');
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(3, 3);
