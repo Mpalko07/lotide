b0330682314e19748c5908d181206547029badb4
Monica Palko <m.a.palko07@gmail.com>
2024-02-17 16:51:00 -0500

    -a

---
+++ test/test_assertArraysEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+
+// TEST CODE
+const arr1 = [1, 2, 3];
+const arr2 = [1, 2, 3];
+
+assertArraysEqual(arr1, arr2);
