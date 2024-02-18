e80c03eec3d01000aeda84e02f077acf353c78c2
Monica Palko <m.a.palko07@gmail.com>
2024-02-17 19:09:14 -0500

    -a

--- test/middleTest.js
+++ test/middleTest.js
@@ -12,18 +12,26 @@ const middle = require('../middle');
 
 describe("#middle", () => {
   it("Returns [2] for [1, 2, 3]", () => {
+    const input = [1, 2, 3];
     assert.deepEqual(middle([1, 2, 3]), [2]);
+    assert.deepEqual(input, [1, 2, 3]);
   });
 
   it("Returns [3] for [1, 2, 3, 4, 5]", () => {
+    const input = [1, 2, 3, 4, 5];
     assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+    assert.deepEqual(input, [1, 2, 3, 4, 5]);
   });
 
   it("Returns [2, 3] for [1, 2, 3, 4]", () => {
+    const input = [1, 2, 3, 4];
     assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+    assert.deepEqual(input, [1, 2, 3, 4]);
   });
 
   it("Returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    const input = [1, 2, 3, 4, 5, 6];
     assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+    assert.deepEqual(input, [1, 2, 3, 4, 5, 6]);
   });
 });
